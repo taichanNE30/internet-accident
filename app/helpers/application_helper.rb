6ff4a1dcf47d1fe1d7eec7ea1f4bdf50aea43dc3
taichanne30 <ne300209@senshu-u.jp>
2020-02-01 01:34:52 +0900

    Add simple style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def tweet_link(link_text, content)
-    link_to link_text, "https://twitter.com/intent/tweet?text=#{CGI.escape(content)}", target: "_blank", rel:%w[noopener noreferrer nofollow]
+  def tweet_link(link_text, content, given_options=nil)
+    html_options, options, name = options, name, block if block_given?
+    options ||= {}
+
+    html_options = convert_options_to_data_attributes(options, html_options)
+    link_to link_text, "https://twitter.com/intent/tweet?text=#{CGI.escape(content)}", target: "_blank", rel:%w[noopener noreferrer nofollow], **given_options
   end
 end
